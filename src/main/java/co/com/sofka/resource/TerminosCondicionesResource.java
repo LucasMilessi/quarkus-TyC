5c05fade2def2aee6e37de42a0bd36309a1e894d
Lucas <lucasmilessi98@gmail.com>
2022-06-03 16:23:02 -0300

    Casi terminado, quedan arreglar algunos errores

--- src/main/java/co/com/sofka/resource/TerminosCondicionesResource.java
+++ src/main/java/co/com/sofka/resource/TerminosCondicionesResource.java
@@ -1,4 +1,58 @@
 package co.com.sofka.resource;
 
+import co.com.sofka.model.TerminosCondiciones;
+import co.com.sofka.model.TerminosCondicionesAceptacion;
+import co.com.sofka.repository.TerminosCondicionesAceptacionRepository;
+import co.com.sofka.repository.TerminosCondicionesRepository;
+import co.com.sofka.service.TerminoCondicionesService;
+import co.com.sofka.service.TerminosCondicionesAceptacionService;
+import io.smallrye.mutiny.Uni;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.NOT_ACCEPTABLE;
+
+@Path("/api/termYcond")
+@Consumes(APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
 public class TerminosCondicionesResource {
+
+    @Inject
+    TerminoCondicionesService terminoCondicionesService;
+
+    @Inject
+    TerminosCondicionesAceptacionService terminosCondicionesAceptacionService;
+
+    @POST
+    public Uni<Response> crearTyC(TerminosCondiciones terminosCondiciones) {
+        return terminoCondicionesService.agregarTerminoCondicion(terminosCondiciones)
+                .map(termsConditions ->Response.ok(termsConditions).build());
+    }
+
+    @GET
+    @Path("/mostrarTyC")
+    @Produces(APPLICATION_JSON)
+    public Uni<Response> mostrarTyC(){
+        return terminoCondicionesService.ultimoTyC()
+                .map(termsConditions -> Response.ok(termsConditions).build());
+    }
+
+    @POST
+    @Path("/agree")
+    @Produces(APPLICATION_JSON)
+    public Uni<Response> crearAcepTyC(TerminosCondicionesAceptacion terminosCondicionesAceptacion) {
+        if(terminosCondicionesAceptacion.getTipoDocumento().equals("Cedula")||
+                terminosCondicionesAceptacion.getTipoDocumento().equals("Pasaporte")){
+
+            return terminosCondicionesAceptacionService.agregarTyCaceptacion(terminosCondicionesAceptacion)
+                    .map(acepTerm -> Response.ok(acepTerm).build())
+                    .onFailure().
+                    recoverWithItem(() -> Response.status(NOT_ACCEPTABLE).build());
+        }
+        return Uni.createFrom().item(Response.status(NOT_ACCEPTABLE).build());
+    }
 }
--- src/main/java/co/com/sofka/service/TerminosCondicionesAceptacionService.java
+++ src/main/java/co/com/sofka/service/TerminosCondicionesAceptacionService.java
@@ -1,16 +1,64 @@
 package co.com.sofka.service;
 
+import co.com.sofka.model.TerminosCondicionesAceptacion;
 import co.com.sofka.repository.TerminosCondicionesAceptacionRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import io.smallrye.mutiny.Uni;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 @ApplicationScoped
 public class TerminosCondicionesAceptacionService {
-
     @Inject
     TerminosCondicionesAceptacionRepository terminosCondicionesAceptacionRepository;
 
+    private final Logger log = LoggerFactory.getLogger(TerminosCondicionesAceptacionService.class);
 
+    public Uni<Object> agregarTyCaceptacion(TerminosCondicionesAceptacion terminosCondicionesAceptacion){
 
+        return Uni.createFrom().item(terminosCondicionesAceptacion)
+                .flatMap(aceptTyC->{
+                    aceptTyC.setFechaDeAceptacion(LocalDate.now());
+                    if (aceptTyC.getTipoDocumento().equals("Pasaporte")){
+                        aceptTyC.setFechaDeAceptacion(LocalDate.now());
+                        return agregarDocumentoPas(aceptTyC);
+                    }
+                    return agregarDocumentoCI(aceptTyC);
+                });
+    }
+
+    public Uni<Object> agregarDocumentoCI(TerminosCondicionesAceptacion terminosCondicionesAceptacion){
+        Pattern patron = Pattern.compile("[0-9]{2}-PN{2}-[0-9]{2}-[0-9]{4}");
+        Matcher matcher = patron.matcher(terminosCondicionesAceptacion.getDocumento());
+        Boolean validar = matcher.matches();
+
+        if (validar){
+            return Uni.createFrom()
+                    .item(terminosCondicionesAceptacion)
+                    .flatMap(terminosCondicionesAceptacionRepository::persist)
+                    .map(termCI -> "Se acepto el termino y condición para el documento "+terminosCondicionesAceptacion.getDocumento());
+        }
+        throw new IllegalArgumentException();
+    }
+    public Uni<Object> agregarDocumentoPas(TerminosCondicionesAceptacion terminosCondicionesAceptacion){
+        Pattern patron = Pattern.compile("[a-zA-Z0-9-]{5,16}");
+        Matcher matcher = patron.matcher(terminosCondicionesAceptacion.getDocumento());
+        Boolean validar = matcher.matches();
+
+        if (validar){
+            return Uni.createFrom()
+                    .item(terminosCondicionesAceptacion)
+                    .flatMap(terminosCondicionesAceptacionRepository::persist)
+                    .map(termPas -> "Se acepto el termino y condición para el documento "+terminosCondicionesAceptacion.getDocumento());
+        }
+        throw new IllegalArgumentException();
+    }
 }
